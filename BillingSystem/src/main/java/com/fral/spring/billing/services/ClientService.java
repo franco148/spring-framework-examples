a29b61e9a1ca27da5835c20e0c774390daa394b9
franco-extreme <franck_ral@hotmail.com>
2018-07-05 09:45:25 -0400

    Java-SpringFramework-BillingSystem: Defined services for Clients and Upload files features.

---
+++ BillingSystem/src/main/java/com/fral/spring/billing/services/ClientService.java
@@ -0,0 +1,17 @@
+package com.fral.spring.billing.services;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.fral.spring.billing.models.entity.Client;
+
+public interface ClientService {
+	
+	List<Client> findAll();
+	Page<Client> findAll(Pageable pageable);
+	void save(Client client);
+	Client findOne(Long id);
+	void delete(Long id);
+}
