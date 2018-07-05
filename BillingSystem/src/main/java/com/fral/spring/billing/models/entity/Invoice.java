6a338b85ae71c80c436333dbf5302922a466c182
franco-extreme <franck_ral@hotmail.com>
2018-07-05 08:49:43 -0400

    Java-SpringFramework-BillingSystem: Adding the required entities of the project.

---
+++ BillingSystem/src/main/java/com/fral/spring/billing/models/entity/Invoice.java
@@ -0,0 +1,93 @@
+package com.fral.spring.billing.models.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Invoice implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+	private String observation;
+	@Temporal(TemporalType.DATE)
+	private Date createdAt;
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Client client;
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="invoice_id")
+	private List<InvoiceItem> invoiceItems;
+	
+	
+	public Invoice() {
+		this.invoiceItems = new ArrayList<>();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getObservation() {
+		return observation;
+	}
+	public void setObservation(String observation) {
+		this.observation = observation;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	public void addItem(InvoiceItem item) {
+		this.invoiceItems.add(item);
+	}
+	
+	public Double getTotal() {
+		Double total = 0.0;
+		
+		for (InvoiceItem invoiceItem : invoiceItems) {
+			total += invoiceItem.calculateAmount();
+		}
+		
+		return total;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = new Date();
+	}
+}
