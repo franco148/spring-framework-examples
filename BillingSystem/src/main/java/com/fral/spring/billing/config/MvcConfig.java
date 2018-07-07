c13c8032902e06aa542969bbbe1f59dfb10f0378
franco-extreme <franck_ral@hotmail.com>
2018-07-06 22:26:56 -0400

    Java-SpringFramework-BillingSystem: Updated the package for MvcConfig class.

--- BillingSystem/src/main/java/com/fral/spring/billing/config/MvcConfig.java
+++ BillingSystem/src/main/java/com/fral/spring/billing/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.fral.spring.billing.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
