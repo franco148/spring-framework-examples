a715e2923bae40b7c4d423d5bda836deeb18bed5
franco-extreme <franck_ral@hotmail.com>
2018-07-10 23:36:03 -0400

    Java-SpringFramework-BillingSystem: Updated the sql file for creating the inicial data of the project. Product table was not properly named in the sql file.

--- BillingSystem/src/main/resources/import.sql
+++ BillingSystem/src/main/resources/import.sql
@@ -26,10 +26,10 @@ INSERT INTO clients (name, last_name, email, created_at, photo) VALUES('John', '
 INSERT INTO clients (name, last_name, email, created_at, photo) VALUES('Richard', 'Roe', 'stiles.roe@gmail.com', '2017-08-25', '');
 
 /* Populate tabla productos */
-INSERT INTO product (name, price, created_at) VALUES('Panasonic Pantalla LCD', 259990, NOW());
-INSERT INTO product (name, price, created_at) VALUES('Sony Camara digital DSC-W320B', 123490, NOW());
-INSERT INTO product (name, price, created_at) VALUES('Apple iPod shuffle', 1499990, NOW());
-INSERT INTO product (name, price, created_at) VALUES('Sony Notebook Z110', 37990, NOW());
-INSERT INTO product (name, price, created_at) VALUES('Hewlett Packard Multifuncional F2280', 69990, NOW());
-INSERT INTO product (name, price, created_at) VALUES('Bianchi Bicicleta Aro 26', 69990, NOW());
-INSERT INTO product (name, price, created_at) VALUES('Mica Comoda 5 Cajones', 299990, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Panasonic Pantalla LCD', 259990, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Sony Camara digital DSC-W320B', 123490, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Apple iPod shuffle', 1499990, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Sony Notebook Z110', 37990, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Hewlett Packard Multifuncional F2280', 69990, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Bianchi Bicicleta Aro 26', 69990, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Mica Comoda 5 Cajones', 299990, NOW());
