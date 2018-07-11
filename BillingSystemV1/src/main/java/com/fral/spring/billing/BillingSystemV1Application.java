f3a609e9ffaf549dbc6b33ed20d7ed10a493b357
franco-extreme <franck_ral@hotmail.com>
2018-07-11 19:20:56 -0400

    Java-SpringFramework-BillingSystemV1: Initial structure of the project.

---
+++ BillingSystemV1/src/main/java/com/fral/spring/billing/BillingSystemV1Application.java
@@ -0,0 +1,12 @@
+package com.fral.spring.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingSystemV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingSystemV1Application.class, args);
+	}
+}
