96283f0467978aa2c6097abfabf2d15b134f6977
franco-extreme <franck_ral@hotmail.com>
2018-07-12 11:39:20 -0400

    Java-SpringFramework-BillingSystemV1: Implemented RedirectAttributes for displaying messages in the UI.

--- BillingSystemV1/src/main/java/com/fral/spring/billing/controllers/ClientController.java
+++ BillingSystemV1/src/main/java/com/fral/spring/billing/controllers/ClientController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fral.spring.billing.models.Client;
 import com.fral.spring.billing.services.ClientService;
@@ -31,25 +32,29 @@ public class ClientController {
 		return "listar";
 	}
 
-	//@RequestMapping(value = "/form")
 	@GetMapping("/form")
 	public String crear(Map<String, Object> model) {
 
 		Client cliente = new Client();
 		model.put("client", cliente);
-		model.put("title", "Client Form");
+		model.put("title", "Create Client");
 		return "form";
 	}
 	
-	//@RequestMapping(value="/form/{id}")
 	@GetMapping("/form/{id}")
-	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 		
 		Client cliente = null;
 		
 		if(id > 0) {
 			cliente = clientService.findOne(id);
+			if(cliente == null) {
+				flash.addFlashAttribute("error", "The client ID does not exist in the system!");
+				return "redirect:/listar";
+			}
+
 		} else {
+			flash.addFlashAttribute("error", "The client ID can not be zero!");
 			return "redirect:/listar";
 		}
 		model.put("client", cliente);
@@ -57,26 +62,29 @@ public class ClientController {
 		return "form";
 	}
 
-	//@RequestMapping(value = "/form", method = RequestMethod.POST)
 	@PostMapping("/form")
-	public String guardar(@Valid Client cliente, BindingResult result, Model model, SessionStatus status) {
+	public String guardar(@Valid Client cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
 
 		if (result.hasErrors()) {
 			model.addAttribute("title", "Client Form");
 			return "form";
 		}
+		
+		String flashMessage = (cliente.getId() != null)? "Client has been edited successfully!" : "Client has been created successfully!";
 
 		clientService.save(cliente);
 		status.setComplete();
+		flash.addFlashAttribute("success", flashMessage);
+
 		return "redirect:listar";
 	}
 	
-	//@RequestMapping(value="/eliminar/{id}")
 	@GetMapping("/delete/{id}")
-	public String eliminar(@PathVariable("id") Long id) {
+	public String eliminar(@PathVariable("id") Long id, RedirectAttributes flash) {
 		
 		if(id > 0) {
 			clientService.delete(id);
+			flash.addFlashAttribute("success", "Client deleted successfully!");
 		}
 		return "redirect:/listar";
 	}
