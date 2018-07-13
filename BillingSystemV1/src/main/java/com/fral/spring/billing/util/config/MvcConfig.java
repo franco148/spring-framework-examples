164e7f0be2029bc219c64f05aca27befd6f63cf4
franco-extreme <franck_ral@hotmail.com>
2018-07-12 23:31:59 -0400

    Java-SpringFramework-BillingSystemV1-Images: Updated MvcConfig file as a second approach for uploading images in the application.

--- BillingSystemV1/src/main/java/com/fral/spring/billing/util/config/MvcConfig.java
+++ BillingSystemV1/src/main/java/com/fral/spring/billing/util/config/MvcConfig.java
@@ -1,5 +1,9 @@
 package com.fral.spring.billing.util.config;
 
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -7,13 +11,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// TODO Auto-generated method stub
-		super.addResourceHandlers(registry);
-		registry.addResourceHandler("/uploads/**")
-		.addResourceLocations("file:/D:/CODE/TMP/SpringFrameworkCourse/uploads/");
+		// FIRST VERSION: Uploads to external directory
+//		super.addResourceHandlers(registry);
+//		registry.addResourceHandler("/uploads/**")
+//		.addResourceLocations("file:/D:/CODE/TMP/SpringFrameworkCourse/uploads/");
 		
+		// SECOND VERSION: Absolute and external directory - uploads folder in the root of the project.
+		super.addResourceHandlers(registry);
+		
+		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+		log.info(resourcePath);
+		
+		registry.addResourceHandler("/uploads/**")
+		.addResourceLocations(resourcePath);
+
 	}
 
 }
