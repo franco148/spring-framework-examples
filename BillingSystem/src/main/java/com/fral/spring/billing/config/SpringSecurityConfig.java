4d8b19cc55aafce6be039931e6b61d645f024874
franco-extreme <franck_ral@hotmail.com>
2018-07-15 22:37:16 -0400

    Java-SpringFramework-BillingSystem-SecurityInMemory: Added and overrided configure method for completing the security configuration. Defined /listar as home page as well. (FIRST APPROACH - IN MEMORY APPROACH).

--- BillingSystem/src/main/java/com/fral/spring/billing/config/SpringSecurityConfig.java
+++ BillingSystem/src/main/java/com/fral/spring/billing/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.fral.spring.billing.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
@@ -11,6 +12,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
+			.antMatchers("/ver/**").hasAnyRole("USER")
+			.antMatchers("/uploads/**").hasAnyRole("USER")
+			.antMatchers("/form/**").hasAnyRole("ADMIN")
+			.antMatchers("/delete/**").hasAnyRole("ADMIN")
+			.antMatchers("/invoices/**").hasAnyRole("ADMIN")
+			.anyRequest().authenticated();
+	}
 
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception
