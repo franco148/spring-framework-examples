3933b5c85fe2966f47ed922ff57ced262c2c1512
franco-extreme <franck_ral@hotmail.com>
2018-07-15 22:44:19 -0400

    Java-SpringFramework-BillingSystem-SecurityInMemory: Configured permit all to /login and /logout for all users. (FIRST APPROACH - IN MEMORY APPROACH).

--- BillingSystem/src/main/java/com/fral/spring/billing/config/SpringSecurityConfig.java
+++ BillingSystem/src/main/java/com/fral/spring/billing/config/SpringSecurityConfig.java
@@ -22,7 +22,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/form/**").hasAnyRole("ADMIN")
 			.antMatchers("/delete/**").hasAnyRole("ADMIN")
 			.antMatchers("/invoices/**").hasAnyRole("ADMIN")
-			.anyRequest().authenticated();
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().permitAll()
+			.and()
+			.logout().permitAll();
 	}
 
 	@Autowired
