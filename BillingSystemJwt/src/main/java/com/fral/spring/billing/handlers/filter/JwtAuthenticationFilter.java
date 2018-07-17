99b9d494c07b4c8cd97017eeb173eff6b98ba568
franco-extreme <franck_ral@hotmail.com>
2018-07-17 17:08:47 -0400

    Java-SpringFramework-BillingSystem-JWT: Generating JWT token.

--- BillingSystemJwt/src/main/java/com/fral/spring/billing/handlers/filter/JwtAuthenticationFilter.java
+++ BillingSystemJwt/src/main/java/com/fral/spring/billing/handlers/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,11 @@
 package com.fral.spring.billing.handlers.filter;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,8 +13,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 	
@@ -50,6 +62,30 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
 		
 		return authenticationManager.authenticate(authToken);
+	}
+
+
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+
+		String userName = authResult.getName(); // or (((User)auth.getPrincipal()).getUsername());
+		String token = Jwts.builder()
+						   .setSubject(userName)
+						   .signWith(SignatureAlgorithm.HS512, "My.Jwt.Secret.Key".getBytes())
+						   .compact();
+		
+		response.addHeader("Authorization", "Bearer ".concat(token));
+		
+		Map<String, Object> body = new HashMap<>();
+		body.put("token", token);
+		body.put("user", (User)authResult.getPrincipal());
+		body.put("message", String.format("Hi %s, you have logged in successfully!", userName));
+		
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(200);
+		response.setContentType("application/json");
 	}
 
 }
