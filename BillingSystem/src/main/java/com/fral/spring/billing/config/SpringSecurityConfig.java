355fe95e9df27fb26a3ceb3a5fdd628c0f54fca6
franco-extreme <franck_ral@hotmail.com>
2018-07-16 22:42:56 -0400

    Java-SpringFramework-BillingSystem-AuthJDBC: Congifured SpringSecurityConfig to support authentication with JDBC (SECOND APPROACH - AUTHENTICATION JDBC).

--- BillingSystem/src/main/java/com/fral/spring/billing/config/SpringSecurityConfig.java
+++ BillingSystem/src/main/java/com/fral/spring/billing/config/SpringSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.fral.spring.billing.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,6 +22,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private LoginSuccessHandler successHandler;
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -52,11 +61,18 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	{
 		//1. FIRST APPROACH. IN MEMORY AUTHENTICATION
 		//UserBuilder users = User.withDefaultPasswordEncoder(); //Deprecated
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+//		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+//		
+//		build.inMemoryAuthentication()
+//			 .withUser(users.username("admin").password("password").roles("ADMIN", "USER"))
+//			 .withUser(users.username("franco").password("password").roles("USER"));
 		
-		build.inMemoryAuthentication()
-			 .withUser(users.username("admin").password("password").roles("ADMIN", "USER"))
-			 .withUser(users.username("franco").password("password").roles("USER"));
+		//2. SECOND APPROACH. AUTHENTICATION JDBC
+		build.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(passwordEncoder)
+		.usersByUsernameQuery("select username, password, enabled from users where username=?")
+		.authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
 	}
 }
