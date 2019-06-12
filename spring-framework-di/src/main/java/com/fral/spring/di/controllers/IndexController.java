bcd2b180465c1a63e240e9c017aae41b39b59958
franco-extreme <franck_ral@hotmail.com>
2019-06-11 21:00:09 -0400

    Java-SpringFramework-DependencyInjection: Getting started.

---
+++ spring-framework-di/src/main/java/com/fral/spring/di/controllers/IndexController.java
@@ -0,0 +1,25 @@
+package com.fral.spring.di.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fral.spring.di.services.MyService;
+
+@Controller
+public class IndexController {
+
+	// The following line is not taking into account DI.
+//	private MyService myService = new MyService();
+	
+	// The following line is taking into account DI
+	@Autowired
+	private MyService myService;
+	
+	@GetMapping({"/", "", "index"})
+	public String index(Model model) {
+		model.addAttribute("object", myService.operation());
+		return "index";
+	}
+}
---
+++ spring-framework-di/src/main/java/com/fral/spring/di/services/MyService.java
@@ -0,0 +1,18 @@
+package com.fral.spring.di.services;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Here we can use an spring stereotype like @component which is the general one,
+ * or a specific one like @service
+ * 
+ * @author Franco
+ *
+ */
+@Component
+public class MyService {
+
+	public String operation() {
+		return "Executing some important process.";
+	}
+}
