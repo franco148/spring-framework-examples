eb9c6ec197e47ec5016673c596c8aadfa2e9f9a8
Franco Arratia <franck_ral@hotmail.com>
2020-03-05 14:56:44 -0400

    spring-webflux-demo: Mono - How it works and write Junit Tests.

--- spring-webflux-demo/src/test/java/com/fral/webflux/webfluxdemo/fluxandmonoplayground/FluxAndMonoTest.java
+++ spring-webflux-demo/src/test/java/com/fral/webflux/webfluxdemo/fluxandmonoplayground/FluxAndMonoTest.java
@@ -2,6 +2,7 @@ package com.fral.webflux.webfluxdemo.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
@@ -105,5 +106,25 @@ public class FluxAndMonoTest {
 //                .expectError(RuntimeException.class)
                 .expectErrorMessage("Exception occurred...")
                 .verify();
+    }
+
+    @Test
+    public void monoTest(){
+
+        Mono<String> stringMono = Mono.just("Spring");
+
+        StepVerifier.create(stringMono.log())
+                .expectNext("Spring")
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void monoTest_Error(){
+
+        StepVerifier.create(Mono.error(new RuntimeException("Exception Occurred")).log())
+                .expectError(RuntimeException.class)
+                .verify();
+
     }
 }
