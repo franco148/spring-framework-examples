0b887bc066e35156fa7e60cc312fad8589201f21
franco-extreme <franck_ral@hotmail.com>
2020-03-24 19:36:16 -0400

    spring-webflux-demo: Reactive programming with DB - Spring profiles.

--- spring-webflux-demo/src/main/java/com/fral/webflux/webfluxdemo/SpringWebfluxDemoApplication.java
+++ spring-webflux-demo/src/main/java/com/fral/webflux/webfluxdemo/SpringWebfluxDemoApplication.java
@@ -3,6 +3,10 @@ package com.fral.webflux.webfluxdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * gradle build -x test : Ignore the tests.
+ * java -jar -Dspring.profiles.active=<profile> <jar-file-path>
+ */
 @SpringBootApplication
 public class SpringWebfluxDemoApplication {
 
