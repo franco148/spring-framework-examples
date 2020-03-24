1fca4e724f3c4f7894b87d9c90116fec81de3158
franco-extreme <franck_ral@hotmail.com>
2020-03-24 19:55:07 -0400

    spring-webflux-demo: Reactive programming with DB - Reactive Mongo Repository operations using JUNIT part1.

---
+++ spring-webflux-demo/src/test/java/com/fral/webflux/webfluxdemo/repository/ItemReactiveRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.fral.webflux.webfluxdemo.repository;
+
+import com.fral.webflux.webfluxdemo.document.Item;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@DataMongoTest
+@RunWith(SpringRunner.class)
+public class ItemReactiveRepositoryTest {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    List<Item> itemList = Arrays.asList(new Item(null,"Samsung TV",400.0),
+            new Item(null,"LG TV",420.0),
+            new Item(null,"Apple Watch",299.99),
+            new Item(null,"Beats Headphones",149.99));
+
+    @Before
+    public void setUp(){
+
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(itemList))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext((item -> {
+                    System.out.println("Inserted Item is :" + item);
+                }))
+                .blockLast();
+
+    }
+
+
+    @Test
+    public void getAllItems(){
+
+        StepVerifier.create(itemReactiveRepository.findAll()) // 4
+                .expectSubscription()
+                .expectNextCount(4)
+                .verifyComplete();
+    }
+}
