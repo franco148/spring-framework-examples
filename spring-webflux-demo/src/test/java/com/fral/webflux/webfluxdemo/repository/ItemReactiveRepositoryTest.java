704db9e664cc581aa5681fbd7867264107cf4db6
franco-extreme <franck_ral@hotmail.com>
2020-03-27 23:16:28 -0400

    spring-webflux-demo: Reactive programming with DB - Fixed unit tests and Build Artifact.

--- spring-webflux-demo/src/test/java/com/fral/webflux/webfluxdemo/repository/ItemReactiveRepositoryTest.java
+++ spring-webflux-demo/src/test/java/com/fral/webflux/webfluxdemo/repository/ItemReactiveRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -18,6 +19,7 @@ import static org.junit.Assert.*;
 
 @DataMongoTest
 @RunWith(SpringRunner.class)
+@DirtiesContext
 public class ItemReactiveRepositoryTest {
 
     @Autowired
