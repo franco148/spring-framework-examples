13f8e4acb7246b451ed159bd2eee57fc25299c80
franco-extreme <franck_ral@hotmail.com>
2020-03-27 22:37:34 -0400

    spring-webflux-demo: Reactive programming with DB - Reactive Mongo Repository operations using JUNIT - Insert.

--- spring-webflux-demo/src/test/java/com/fral/webflux/webfluxdemo/repository/ItemReactiveRepositoryTest.java
+++ spring-webflux-demo/src/test/java/com/fral/webflux/webfluxdemo/repository/ItemReactiveRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
@@ -69,5 +70,17 @@ public class ItemReactiveRepositoryTest {
                 .expectSubscription()
                 .expectNextCount(1)
                 .verifyComplete();
+    }
+
+    @Test
+    public void saveItem(){
+
+        Item item = new Item(null,"Google Home Mini",30.00);
+        Mono<Item> savedItem = itemReactiveRepository.save(item);
+        StepVerifier.create(savedItem.log("saveItem : "))
+                .expectSubscription()
+                .expectNextMatches(item1 -> (item1.getId()!=null && item1.getDescription().equals("Google Home Mini")))
+                .verifyComplete();
+
     }
 }
