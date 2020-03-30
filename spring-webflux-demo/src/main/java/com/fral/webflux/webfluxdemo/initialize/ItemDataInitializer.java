7dfa57373ecf41d44ca399c4d5a9c184bce2fbcd
franco-extreme <franck_ral@hotmail.com>
2020-03-30 19:14:28 -0400

    spring-webflux-demo: Section 13 - Build the item reactive API Endpoint Using Rest Controller - JUNIT Test using WebTestClient - Part1

--- spring-webflux-demo/src/main/java/com/fral/webflux/webfluxdemo/initialize/ItemDataInitializer.java
+++ spring-webflux-demo/src/main/java/com/fral/webflux/webfluxdemo/initialize/ItemDataInitializer.java
@@ -5,6 +5,7 @@ import com.fral.webflux.webfluxdemo.document.Item;
 import com.fral.webflux.webfluxdemo.repository.ItemReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
@@ -12,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
+@Profile("!test")
 public class ItemDataInitializer implements CommandLineRunner {
 
     @Autowired
