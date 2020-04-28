933712a684242db73ca5c45c47cf6e5a0d4c6866
francofral <franck_ral@hotmail.com>
2020-04-27 23:13:00 -0400

    spring-webflux-demo: Section 19 - Streaming Real Time Data Using Server Side Events (SSE) - Setting up the capped collection (MongoDB)

---
+++ spring-webflux-reactive/src/main/java/com/learnreactivespring/initialize/ItemDataInitializer.java
@@ -0,0 +1,61 @@
+package com.learnreactivespring.initialize;
+
+import com.learnreactivespring.document.Item;
+import com.learnreactivespring.document.ItemCapped;
+import com.learnreactivespring.repository.ItemReactiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.CollectionOptions;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@Profile("!test")
+public class ItemDataInitializer implements CommandLineRunner {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    @Autowired
+    MongoOperations mongoOperations;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        initalDataSetUp();
+        createCappedCollection();
+    }
+
+    private void createCappedCollection() {
+        mongoOperations.dropCollection(ItemCapped.class);
+        mongoOperations.createCollection(ItemCapped.class, CollectionOptions.empty().maxDocuments(20).size(50000).capped());
+
+    }
+
+    public List<Item> data() {
+
+        return Arrays.asList(new Item(null, "Samsung TV", 399.99),
+                new Item(null, "LG TV", 329.99),
+                new Item(null, "Apple Watch", 349.99),
+                new Item("ABC", "Beats HeadPhones", 149.99));
+    }
+
+
+    private void initalDataSetUp() {
+
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                        .flatMap(itemReactiveRepository::save)
+                        .thenMany(itemReactiveRepository.findAll())
+                        .subscribe((item -> {
+                            System.out.println("Item inserted from CommandLineRunner : " + item);
+                        }));
+
+    }
+
+}
